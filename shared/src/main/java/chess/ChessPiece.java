346ce2f547f6e14b4f7bdb14c87d4e53e04c7c4f
Huey <huey@kolowich.org>
2024-01-22 23:58:31 -0700

    Added rook move functionality

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -55,7 +55,7 @@ public class ChessPiece {
         HashSet<ChessMove> pieceMoves = new HashSet<ChessMove>();
         List<int[]> possibleEndPositions;
 
-        // Could make a separate interface that is a piece moves calculator and have the different pieces inherit from it
+        //TODO build a separate interface that is a piece moves calculator and have the different pieces inherit from it
         switch (type) {
             case BISHOP -> {
                 possibleEndPositions = bishopMoves(board, myPosition);
@@ -71,6 +71,9 @@ public class ChessPiece {
             }
             case QUEEN -> {
                 possibleEndPositions = queenMoves(board, myPosition);
+            }
+            case ROOK -> {
+                possibleEndPositions = rookMoves(board, myPosition);
             }
             default -> {
                 return new ArrayList<>();
@@ -197,6 +200,48 @@ public class ChessPiece {
             possibleEndPositions.add(new int[]{queenPosition.getRow(), c});
 
             if (board.isEnemyPiece(new ChessPosition(queenPosition.getRow(), c), this.pieceColor)) { break; } else if (board.isPiece(new ChessPosition(queenPosition.getRow(), c))) {
+                possibleEndPositions.removeLast();
+                break;
+            }
+        }
+
+        return possibleEndPositions;
+    }
+
+    private List<int[]> rookMoves(ChessBoard board, ChessPosition rookPosition) {
+        List<int[]> possibleEndPositions = new ArrayList<>();
+
+        for (int r = rookPosition.getRow() + 1; (r < 9); r++) {
+            possibleEndPositions.add(new int[]{r, rookPosition.getColumn()});
+
+            if (board.isEnemyPiece(new ChessPosition(r, rookPosition.getColumn()), this.pieceColor)) { break; } else if (board.isPiece(new ChessPosition(r, rookPosition.getColumn()))) {
+                possibleEndPositions.removeLast();
+                break;
+            }
+        }
+
+        for (int r = rookPosition.getRow() - 1; (r > 0); r--) {
+            possibleEndPositions.add(new int[]{r, rookPosition.getColumn()});
+
+            if (board.isEnemyPiece(new ChessPosition(r, rookPosition.getColumn()), this.pieceColor)) { break; } else if (board.isPiece(new ChessPosition(r, rookPosition.getColumn()))) {
+                possibleEndPositions.removeLast();
+                break;
+            }
+        }
+
+        for (int c = rookPosition.getColumn() + 1; (c < 9); c++) {
+            possibleEndPositions.add(new int[]{rookPosition.getRow(), c});
+
+            if (board.isEnemyPiece(new ChessPosition(rookPosition.getRow(), c), this.pieceColor)) { break; } else if (board.isPiece(new ChessPosition(rookPosition.getRow(), c))) {
+                possibleEndPositions.removeLast();
+                break;
+            }
+        }
+
+        for (int c = rookPosition.getColumn() - 1; (c > 0); c--) {
+            possibleEndPositions.add(new int[]{rookPosition.getRow(), c});
+
+            if (board.isEnemyPiece(new ChessPosition(rookPosition.getRow(), c), this.pieceColor)) { break; } else if (board.isPiece(new ChessPosition(rookPosition.getRow(), c))) {
                 possibleEndPositions.removeLast();
                 break;
             }
