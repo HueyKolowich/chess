9d71fadc5b2d134b1cb70d5e3f923e9652096200
Huey <huey@kolowich.org>
2024-02-26 14:56:26 -0700

    Unit tests for registrationService register method

---
+++ server/src/test/service/RegistrationServiceTest.java
@@ -0,0 +1,47 @@
+package service;
+
+import chess.model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import service.resultRecords.AuthResult;
+import service.serviceExceptions.UserNameInUseException;
+import spark.Response;
+
+import java.net.HttpURLConnection;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RegistrationServiceTest {
+
+    @Test
+    public void register() throws UserNameInUseException {
+        /*Register
+
+        property	        value
+        Description	        Register a new user.
+        URL path	        /user
+        HTTP Method	        POST
+        Body	            { "username":"", "password":"", "email":"" }
+        Success response	[200] { "username":"", "authToken":"" }
+        Failure response	[400] { "message": "Error: bad request" }
+        Failure response	[403] { "message": "Error: already taken" }
+        Failure response	[500] { "message": "Error: description" }
+        */
+
+        //Initialization
+        RegistrationService registrationService = new RegistrationService();
+        UserData testUser1 = new UserData("TestUsername1", "TestPassword1", "TestEmail1@email.com");
+        UserData testUser1copy = new UserData("TestUsername1", "TestPassword1.1", "TestEmail1.1@email.com");
+
+        //Positive case
+        AuthResult registerResponse = registrationService.register(testUser1);
+        AuthResult expectedResponse = new AuthResult("TestUsername1", "");
+
+        Assertions.assertEquals(expectedResponse.username(), registerResponse.username());
+        Assertions.assertNotNull(registerResponse.authToken());
+
+        //Negative case
+        Assertions.assertThrows(UserNameInUseException.class, () -> registrationService.register(testUser1));
+    }
+}
