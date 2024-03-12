43f6a6ceccabb416a14c7c6f941dc60c358a7154
Huey <huey@kolowich.org>
2024-03-12 00:12:58 -0600

    Switched clear service to Database Dao

--- server/src/main/java/service/ClearService.java
+++ server/src/main/java/service/ClearService.java
@@ -3,9 +3,18 @@ package service;
 import dataAccess.*;
 
 public class ClearService {
-    private final UserDao userDao = new MemoryUserDao();
+    private final UserDao userDao;
     private final AuthDao authDao = new MemoryAuthDao();
     private final GameDao gameDao = new MemoryGameDao();
+
+    {
+        try {
+            userDao = new DatabaseUserDao();
+        } catch (DataAccessException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public void delete() throws DataAccessException {
         userDao.clear();
         authDao.clear();
