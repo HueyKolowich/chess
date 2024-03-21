2b2ab7b58a8c664bb742244465caedb8170242b5
Huey <huey@kolowich.org>
2024-03-21 00:01:08 -0600

    Main loop for client

---
+++ client/src/main/java/client/ChessClient.java
@@ -0,0 +1,7 @@
+package client;
+
+public class ChessClient {
+    public ChessClient(String serverUrl) {}
+
+    public String eval(String line) { return null; }
+}
---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,25 @@
+package client;
+
+import java.util.Scanner;
+
+public class ServerFacade {
+    private final ChessClient client;
+
+    public ServerFacade(String serverUrl) {
+        this.client = new ChessClient(serverUrl);
+    }
+
+    public void run() {
+        System.out.println("Welcome to 240 chess. Type help to get started.");
+
+        Scanner scanner = new Scanner(System.in);
+        String result = "";
+        String line;
+
+        while (!result.equals("quit")) {
+            line = scanner.nextLine();
+
+            result = client.eval(line);
+        }
+    }
+}
