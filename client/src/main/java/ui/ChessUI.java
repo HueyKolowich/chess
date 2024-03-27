ae8c27f9e29c2af546d069347808c44325271e29
Huey <huey@kolowich.org>
2024-03-27 17:36:01 -0600

    Finilized no-state chessboard UI

--- client/src/main/java/ui/ChessUI.java
+++ client/src/main/java/ui/ChessUI.java
@@ -2,40 +2,40 @@ package ui;
 
 import java.io.PrintStream;
 import java.nio.charset.StandardCharsets;
-import java.util.Random;
 
 import static ui.EscapeSequences.*;
 
 public class ChessUI {
-
     private static final int BOARD_SIZE_IN_SQUARES = 8;
     private static final int SQUARE_SIZE_IN_CHARS = 3;
-    private static final int LINE_WIDTH_IN_CHARS = 1;
+    private static final String[] horizontalHeadersOrientation1 = {" ", "h", "g", "f", "e", "d", "c", "b", "a", " "};
+    private static final String[] horizontalHeadersOrientation2 = {" ", "a", "b", "c", "d", "e", "f", "g", "h", " "};
+    private static final String[] verticalHeadersOrientation1 = {"1", "2", "3", "4", "5", "6", "7", "8"};
+    private static final String[] verticalHeadersOrientation2 = {"8", "7", "6", "5", "4", "3", "2", "1"};
     private static final String EMPTY = " ";
-    private static final String X = "X";
-    private static final String O = "O";
-    private static Random rand = new Random();
-
 
     public static void main(String[] args) {
         var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
 
         out.print(ERASE_SCREEN);
 
-        drawHeaders(out);
+        drawHeaders(out, horizontalHeadersOrientation1);
+        drawChessBoard(out, verticalHeadersOrientation1);
+        drawHeaders(out, horizontalHeadersOrientation1);
 
-        drawChessBoard(out);
+        out.println();
 
-        drawHeaders(out);
+        drawHeaders(out, horizontalHeadersOrientation2);
+        drawChessBoard(out, verticalHeadersOrientation2);
+        drawHeaders(out, horizontalHeadersOrientation2);
 
         out.print(SET_BG_COLOR_BLACK);
         out.print(SET_TEXT_COLOR_WHITE);
     }
 
-    private static void drawHeaders(PrintStream out) {
+    private static void drawHeaders(PrintStream out, String[] headers) {
         setBlack(out);
 
-        String[] headers = {" ", "h", "g", "f", "e", "d", "c", "b", "a", " "};
         for (int boardCol = 0; boardCol < BOARD_SIZE_IN_SQUARES + 2; ++boardCol) {
             drawHeader(out, headers[boardCol]);
 
@@ -72,9 +72,7 @@ public class ChessUI {
         out.print(EMPTY);
     }
 
-    private static void drawChessBoard(PrintStream out) {
-        String[] verticalHeaders = {"1", "2", "3", "4", "5", "6", "7", "8"};
-
+    private static void drawChessBoard(PrintStream out, String[] verticalHeaders) {
         for (int boardRow = 0; boardRow < BOARD_SIZE_IN_SQUARES; ++boardRow) {
 
             drawRowOfSquares(out, boardRow, verticalHeaders[boardRow]);
